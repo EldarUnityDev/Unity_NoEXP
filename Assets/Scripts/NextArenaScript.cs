9f731644fdeb5c916767e2489a7eaf63013ad081
EldarUnityDev <eldar.teaching@gmail.com>
2025-01-20 23:18:38 +0300

    dirty level transition

--- Assets/Scripts/NextArenaScript.cs
+++ Assets/Scripts/NextArenaScript.cs
@@ -11,15 +11,28 @@ public class ExitAreaScript : MonoBehaviour
     public GameObject currentArena;
     public GameObject nextArena;
     public bool nextArenaSwitch;
+    public bool comeBack;
+    public bool stillThere;
+
+    public float multiplier;
+    public float desiredUp;
+    public float desiredZ;
+    public float oriZ;
+    Vector3 desiredArenaPlace;
 
     void Start()
     {
+        stillThere = true;
         secondsBeforeNextLevel = graceTimeAtEndOfLevel;
+        desiredUp = transform.position.y + 2;
+        desiredArenaPlace = new Vector3(75, 0, -15);
+        desiredZ = -15;
+        oriZ = transform.position.z;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.GetComponentInParent<PlayerBehaviour>() != null)
+        if (other.gameObject.GetComponentInParent<PlayerBehaviour>() != null && stillThere)
         {
             secondsBeforeNextLevel -= Time.deltaTime;
             if (secondsBeforeNextLevel <= 0)
@@ -40,9 +53,26 @@ public class ExitAreaScript : MonoBehaviour
     {
         if (nextArenaSwitch)
         {
-            //z = -25
+            if(transform.position.y < desiredUp)
+            {
+                transform.position += new Vector3(0, 4, 0) * Time.deltaTime;
+                References.thePlayer.transform.position += new Vector3(0, 4, 0) * Time.deltaTime;
+            }
             
-            nextArena.transform.position -= new Vector3(0,0,9) * Time.deltaTime;
+            if(nextArena.transform.position.z > desiredZ)
+            {
+                nextArena.transform.position -= new Vector3(0, 0, 15) * Time.deltaTime;
+                currentArena.transform.position -= new Vector3(0, 0, 15) * Time.deltaTime;
+
+            }
+            if(nextArena.transform.position.z < desiredZ)
+            {
+                //nextArena.transform.position = desiredArenaPlace;
+                nextArenaSwitch = false;
+                stillThere = false;
+                transform.position -= new Vector3(0, 2, 0);
+                References.thePlayer.transform.position -= new Vector3(0, 2, 0);
+            }
         }
     }
 }
